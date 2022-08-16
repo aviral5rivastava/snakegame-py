ba789f6ccf951d11670a3af86bcae64cb280d272
aviral5rivastava <aviral.lonewolf@gmail.com>
2022-08-17 00:31:56 +0530

    mov_forward method added to snake class

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(height=600, width=600)
 screen.bgcolor("black")
 screen.title("Snake")
+screen.tracer(0)
 
 
 class Snake:
@@ -12,12 +14,27 @@ class Snake:
         for i in range(0, 3):
             bit = Turtle("square")
             bit.color("white")
+            bit.penup()
             self.all_bits.append(bit)
         x = -10
         for bit in self.all_bits:
             bit.goto(x, 0)
             x += 10
 
+    def mov_forward(self):
+        for i in range(len(self.all_bits)-1, 0, -1):
+            new_x = self.all_bits[i - 1].xcor()
+            new_y = self.all_bits[i - 1].ycor()
+            self.all_bits[i].goto(new_x, new_y)
+        self.all_bits[0].forward(20)
+
 
 snake = Snake()
+is_game_on = True
+
+while is_game_on:
+    screen.update()
+    time.sleep(0.25)
+    snake.mov_forward()
+
 screen.exitonclick()
